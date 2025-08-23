73df7948d9c8816869f3217659b9a4c7008beccc
vihackeriue <talavyfc12345678@gmail.com>
2025-08-23 10:27:38 +0700

    first commit backend

---
+++ wain-voting-backend/src/test/java/com/wain/wainvotingbackend/WainVotingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wain.wainvotingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WainVotingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
